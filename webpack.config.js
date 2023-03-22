48e12ece052013510ab33a4ddfbffc13be55dd75
Mohammed-Galal <mohammedgalal682@gmail.com>
2023-03-22 19:40:50 +0200

    bugs fixed

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,31 @@
 #!/usr/bin/env node
 
+const path = require("path"),
+  rootPath = process.cwd(),
+  webpack = require("webpack");
+
 const args = process.argv,
-  envMode = args[2] === "development" ? 1 : 2,
-  isDev = envMode === 1,
-  targetApp = args[3] || (!isDev ? envMode : undefined);
+  isDev = args[2] === "serve",
+  targetApp = args[3] || (isDev ? undefined : args[2]);
 
 if (targetApp === undefined) {
   const targetErr = new Error();
   targetErr.name = "Undefined Target".toUpperCase();
   targetErr.message = "please specify an <AppName> to start";
   throw targetErr;
+} else if (isDev) {
+  const nodemon = require("nodemon");
+  return nodemon({
+    cwd: path.resolve(rootPath, targetApp),
+    ignore: ["node_modules/**"],
+  });
 }
-
-const nodemon = require("nodemon"),
-  path = require("path"),
-  rootPath = process.cwd(),
-  webpack = require("webpack");
 
 const config = {},
   modes = ["none", "development", "production"],
   targets = ["node", "web"];
 
-config.mode = modes[envMode];
+config.mode = modes[2];
 config.target = targets[0];
 config.entry = {
   index: path.resolve(rootPath, targetApp, "config.js"),
@@ -30,10 +34,8 @@ config.entry = {
 config.output = {
   path: path.resolve(rootPath, targetApp),
   chunkFormat: "commonjs",
-  filename: isDev
-    ? "temp.js"
-    : ({ chunk }) =>
-        (chunk.name === "index" ? "index" : "modules/[name]") + ".js",
+  filename: ({ chunk }) =>
+    (chunk.name === "index" ? "index" : "modules/[name]") + ".js",
 };
 
 config.resolve = {
@@ -44,11 +46,11 @@ config.resolve = {
 
 config.optimization = {
   chunkIds: "named",
-  splitChunks: !isDev && {
+  splitChunks: {
     chunks: "all",
     cacheGroups: {
       module: {
-        test: /[\\/]node_modules[\\/]/,
+        test: /[\\/]node_modules(?![\\/]envase)[\\/]/,
       },
     },
   },
@@ -56,14 +58,7 @@ config.optimization = {
 
 try {
   const compiler = webpack(config);
-  compiler.run(watchCallback);
+  compiler.run();
 } catch {
   console.log(config);
-}
-
-function watchCallback() {
-  isDev &&
-    nodemon(targetApp + "/temp.js", {
-      ignore: ["node_modules/**", __dirname + __filename],
-    });
 }
